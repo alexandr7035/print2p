14c5f588ee3adf9408b500fe8159f1d9a262598f
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-03-21 00:05:20 +0300

    Empty swing window created

---
+++ src/main/java/com/alexandr7035/print2p/Main.java
@@ -0,0 +1,11 @@
+package com.alexandr7035.print2p;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        WindowFrame window = new WindowFrame();
+        window.run();
+    }
+
+}
