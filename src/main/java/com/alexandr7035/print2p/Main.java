8b167c85ad8d3e941ed40c7ee05d0b314f4620a2
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-03-21 00:50:37 +0300

    Refuse from swing. Move to JavaFX

--- src/main/java/com/alexandr7035/print2p/Main.java
+++ src/main/java/com/alexandr7035/print2p/Main.java
@@ -1,11 +1,37 @@
 package com.alexandr7035.print2p;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
 
-    public static void main(String args[]) {
+ Scene scene = new Scene(root, 300, 250);
 
-        WindowFrame window = new WindowFrame();
-        window.run();
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
+ public static void main(String[] args) {
+        launch(args);
+    }
 }
