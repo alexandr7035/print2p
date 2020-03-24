d6247c6fafe83022e7b600767e085a8765823ed0
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-03-24 17:19:44 +0300

    Add empty PrintWrapper class

---
+++ src/main/java/com/alexandr7035/print2p/PrintWrapper.java
@@ -0,0 +1,16 @@
+package com.alexandr7035.print2p;
+
+public class PrintWrapper {
+
+    private Document printedDoc;
+
+    public void printFirst() {
+
+    }
+
+    public void printSecond() {
+
+    }
+
+}
+
