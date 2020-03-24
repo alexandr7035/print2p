e3eaf633c563da52a7b3f7cd5a521673cdf9cd1e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-03-24 17:16:33 +0300

    Doc directory inside WORKING_DIR is screated now

--- src/main/java/com/alexandr7035/print2p/Settings.java
+++ src/main/java/com/alexandr7035/print2p/Settings.java
@@ -4,13 +4,21 @@ import java.io.File;
 
 public final class Settings {
 
-    public static final File WORKING_DIR = new File(System.getProperty("user.home") +
-                                                    File.separator + ".print2p");
+    private static final String WORKING_DIR_PATH = System.getProperty("user.home") +
+                                                     File.separator + ".print2p";
+
+    private static final File WORKING_DIR = new File(WORKING_DIR_PATH);
+
+    private static final File DOC_DIR = new File(WORKING_DIR_PATH + "/doc");
 
     public static void initApp() {
 
         if (! WORKING_DIR.exists()) {
             WORKING_DIR.mkdir();
+        }
+
+        if (! DOC_DIR.exists()) {
+            DOC_DIR.mkdir();
         }
 
     }
